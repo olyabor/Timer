14b90a107d2630c79b195cb53c131113b8659512
Olga Borisenko <olyabor800@gmail.com>
2020-09-08 22:38:38 +0300

    feat(calc): add total animation

--- script/script.js
+++ script/script.js
@@ -272,6 +272,62 @@ window.addEventListener('DOMContentLoaded', function(){
 
   // калькулятор
   const calcInput = document.getElementById('calc').querySelectorAll('input');
+  const calc = (price = 100) => {
+
+    const calcBlock = document.querySelector('.calc-block'),
+      calcType = document.querySelector('.calc-type'),
+      calcSquare = document.querySelector('.calc-square'),
+      calcCount = document.querySelector('.calc-count'),
+      calcDay = document.querySelector('.calc-day'),
+      totalValue = document.getElementById('total');
+
+      const countSum = () => {
+        let total = 0,
+          countValue = 1,
+          dayValue = 1;
+        const typeValue = calcType.options[calcType.selectedIndex].value,
+          squareValue = +calcSquare.value;
+
+        if (calcCount.value > 1) {
+          countValue += (calcCount.value - 1) / 10;
+        }
+
+        if (calcDay.value && calcDay.value < 5){
+          dayValue *= 2;
+        } else if (calcDay.value && calcDay.value < 10) {
+          dayValue *= 1.5;
+        }
+        
+        if (typeValue && squareValue) {
+          total = price * typeValue * squareValue * countValue * dayValue;
+        }
+        
+        if (total) {
+          total = Math.round(total);
+          let interval = setInterval(() => {
+            let start = +totalValue.textContent;
+            if (start === total) {
+              clearInterval(interval);
+            } else if (start < total) {
+              totalValue.textContent = ++start;
+            } else {
+              totalValue.textContent = --start;
+            } 
+          }, 10);
+        }
+      };
+
+      calcBlock.addEventListener('change', (event) => {
+        const target = event.target;
+
+        if (target.matches('select') || target.matches('input')) {
+          countSum();
+        }
+      });
+  };
+
+  calc(100);
+
   calcInput.forEach((item) => {
       item.addEventListener('input', () => {
           item.value = item.value.replace(/\D/g, '');
